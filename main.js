7c2c778ed91797f2131ae61677176f46d09dce9b
Videycoldd <okdokfikri@gmail.com>
2026-05-01 18:02:12 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+/* TOGGLE SHARE */
+const shareBtn = document.getElementById("shareBtn");
+const shareLinks = document.getElementById("shareLinks");
+
+shareBtn.onclick = () => {
+    window.open("https://s.shopee.co.id/5VQokhUqZs", "_blank")
+};
+/* UNDANGAN */
+function joinWhatsAppGroup() {
+    window.open("https://whatsapp.com/channel/0029VbCCtde3WHTdWYywNk2m");
+}
+
+function openFacebookPage() {
+    window.open("https://s.shopee.co.id/AACeJ6gY47");
+}
+const video = document.getElementById("video");
+const overlay = document.getElementById("videoOverlay");
+
+let overlayClicked = false; 
+
+// Overlay muncul di detik tertentu
+video.addEventListener("timeupdate", () => {
+    if (video.currentTime >= 1 && !overlayClicked) {
+        overlay.classList.add("show");
+    }
+});
+
+// Klik overlay
+overlay.addEventListener("click", () => {
+    overlayClicked = true;              
+    overlay.style.display = "none";   
+    overlay.classList.remove("show");
+
+    // Aksi setelah klik
+    window.open("https://s.shopee.co.id/5VQokhUqZs", "_blank");
+});
+
+
+
+
+
+
+
+
